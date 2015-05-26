ce47cffe57dfad65bde8d2296110caf0c9bdd473
Jerry Chang <therealjerrycan@gmail.com>
2015-05-25 18:32:23 -0700

    testing collections in meteor [Jerry Chang]

--- movieapp.js
+++ movieapp.js
@@ -1,26 +1,80 @@
-if (Meteor.isClient) {
+// ********** Collections  start (lib/colelctions.js)  *****************
+  
+  /*  Here we are defining the collection used for this application, 
+       could easily be moved into 'common code' user lib/  */
 
-  Template.body.helpers({
-    // testing out client side template rendering 
-    movies: [
-      {MovieTitle: 'Amelie', ReleaseYear: 1991, Genre: 'Horror', button1: 'Edit!', button2: 'Delete'},
-      {MovieTitle: 'Amelie', ReleaseYear: 1991, Genre: 'Horror', button1: 'Edit!', button2: 'Delete'},
-      {MovieTitle: 'Amelie', ReleaseYear: 1991, Genre: 'Horror', button1: 'Edit!', button2: 'Delete'}
-    ]
+  // Defining the movie collection 
+  Movies = new Mongo.Collection("Movies");
 
-  });
+// ********** Collections end   ****************************************
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-    Tasks = new Mongo.Collection("Movies");
-  });
-}
+// ********** client Side code start (server.js)  **********************
+
+  if (Meteor.isClient) {
+
+    Template.body.helpers({
+      // testing out client side template rendering 
+      MovieSortByCreateDate: function () {
+        // Show newest tasks first
+        return Movies.find({}, {sort: {createdAt: -1}});
+      },
+      MovieSortByAlphabeticalOrd: function(){
+        return Movies.find({}, {sort: {movieTitle: 1}});
+      },
+
+      genre: [
+        {genre: "Horror"},
+        {genre: "Stuff"},
+        {genre: "otherstuff"},
+      ]
+    });
+
+    Template.body.events({
+      'submit .new-movie': function (event) {
+        // This function is called when the new task form is submitted
+        var movieTitle = event.target.movietitle.value;
+        var releaseYear = event.target.releaseYear.value;
+        var genre = event.target.genre.value;
+
+        Movies.insert({
+          movieTitle: movieTitle,
+          releaseYear: releaseYear,
+          genre: genre,
+          createdAt: new Date() // current time
+        });
+
+        // Clear form
+        event.target.movietitle.value = "";
+        event.target.releaseYear.value = "";
+
+        // Prevent default form submit
+        return false;
+      }
+
+    });
+
+    Template.movieDetails.events({
+      
+      "click .edit": function(){
+        console.log(this._id);
+        console.dir(Movies.findOne(this._id));
+
+      }
+
+    })
+  }
+// ********** client Side code end (server.js)  **********************
+
+
+// ********** Server Side code start (server.js)  **********************
+
+  if (Meteor.isServer) {
+    Meteor.startup(function () {
+      // code to run on server at startup
+    });
+  }
+
+// ********** Server Side code end ************************************
+
+
