ce5e4e270e05c1f7f209c68cb27116e45e23573c
Jerry Chang <therealjerrycan@gmail.com>
2015-05-27 10:34:02 -0700

    updated the d3 and nvd3 cdn links [Jerry Chang]

--- public/chart.js
+++ public/chart.js
@@ -1,69 +1,94 @@
 $(function(){
-/*These lines are all chart setup.  Pick and choose which chart features you want to utilize. */
-nv.addGraph(function() {
-  var chart = nv.models.lineChart()
-                .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
-                .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
-                .transitionDuration(350)  //how fast do you want the lines to transition?
-                .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
-                .showYAxis(true)        //Show the y-axis
-                .showXAxis(true)        //Show the x-axis
-  ;
+  nv.addGraph(function() {
+        var chart = nv.models.lineChart();
+        var fitScreen = false;
+        var width = 400;
+        var height = 250;
+        var zoom = 1;
+        var svg = d3.select("body").append("svg");
+        svg.append("rect")
+    .attr("fill", "pink");
+        chart.useInteractiveGuideline(true);
+        chart.xAxis
+            .tickFormat(d3.format(',r'));
+        chart.lines.dispatch.on("elementClick", function(evt) {
+            console.log(evt);
+        });
+        chart.yAxis
+            .axisLabel('Voltage (v)')
+            .tickFormat(d3.format(',.2f'));
+        d3.select('#chart1 svg')
+            .attr('perserveAspectRatio', 'xMinYMid')
+            .attr('width', width)
+            .attr('height', height)
 
-  chart.xAxis     //Chart x-axis settings
-      .axisLabel('Time (ms)')
-      .tickFormat(d3.format(',r'));
-
-  chart.yAxis     //Chart y-axis settings
-      .axisLabel('Voltage (v)')
-      .tickFormat(d3.format('.02f'));
-
-  /* Done setting the chart up? Time to render it!*/
-  var myData = sinAndCos();   //You need data...
-
-  d3.select('#chart svg')    //Select the <svg> element you want to render the chart in.   
-      .datum(myData)         //Populate the <svg> element with chart data...
-      .call(chart);          //Finally, render the chart!
-
-  //Update the chart when window resizes.
-  nv.utils.windowResize(function() { chart.update() });
-  return chart;
-});
-/**************************************
- * Simple test data generator
- */
-function sinAndCos() {
-  var sin = [],sin2 = [],
-      cos = [];
-
-  //Data is represented as an array of {x,y} pairs.
-  for (var i = 0; i < 100; i++) {
-    sin.push({x: i, y: Math.sin(i/10)});
-    sin2.push({x: i, y: Math.sin(i/10) *0.25 + 0.5});
-    cos.push({x: i, y: .5 * Math.cos(i/10)});
-  }
-
-  //Line chart data should be sent as an array of series objects.
-  return [
-    {
-      values: sin,      //values - represents the array of {x,y} data points
-      key: 'Sine Wave', //key  - the name of the series.
-      color: '#ff7f0e'  //color - optional: choose your own line color.
-    },
-    {
-      values: cos,
-      key: 'Cosine Wave',
-      color: '#2ca02c'
-    },
-    {
-      values: sin2,
-      key: 'Another sine wave',
-      color: '#7777ff',
-      area: true      //area - set to true if you want this line to turn into a filled area chart.
+            .datum(sinAndCos());
+        setChartViewBox();
+        resizeChart();
+        nv.utils.windowResize(resizeChart);
+        d3.select('#zoomIn').on('click', zoomIn);
+        d3.select('#zoomOut').on('click', zoomOut);
+        function setChartViewBox() {
+            var w = width * zoom,
+                h = height * zoom;
+            chart
+                .width(w)
+                .height(h);
+            d3.select('#chart1 svg')
+                .attr('viewBox', '0 0 ' + w + ' ' + h)
+                .transition().duration(500)
+                .call(chart);
+        }
+        function zoomOut() {
+            zoom += .25;
+            setChartViewBox();
+        }
+        function zoomIn() {
+            if (zoom <= .5) return;
+            zoom -= .25;
+            setChartViewBox();
+        }
+        // This resize simply sets the SVG's dimensions, without a need to recall the chart code
+        // Resizing because of the viewbox and perserveAspectRatio settings
+        // This scales the interior of the chart unlike the above
+        function resizeChart() {
+            var container = d3.select('#chart1');
+            var svg = container.select('svg');
+            if (fitScreen) {
+                // resize based on container's width AND HEIGHT
+                var windowSize = nv.utils.windowSize();
+                svg.attr("width", windowSize.width);
+                svg.attr("height", windowSize.height);
+            } else {
+                // resize based on container's width
+                var aspect = chart.width() / chart.height();
+                var targetWidth = parseInt(container.style('width'));
+                svg.attr("width", targetWidth);
+                svg.attr("height", Math.round(targetWidth / aspect));
+            }
+        }
+        return chart;
+    });
+    function sinAndCos() {
+        var sin = [],
+            cos = [];
+        for (var i = 0; i < 100; i++) {
+            sin.push({x: i, y: Math.sin(i/10) });
+            cos.push({x: i, y: .5 * Math.cos(i/10)});
+        }
+        return [
+            {
+                values: sin,
+                key: "Sine Wave",
+                color: "#ff7f0e"
+            },
+            {
+                values: cos,
+                key: "Cosine Wave",
+                color: "#2ca02c"
+            }
+        ];
     }
-  ];
-}
-
 
 })
   
