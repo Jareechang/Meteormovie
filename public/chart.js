063f7ceb1aa7600afd67fa38e6ef7ac7f9778d2b
Jerry Chang <therealjerrycan@gmail.com>
2015-05-27 21:47:44 -0700

    finished data and chart rendering with analytics [Jerry Chang]

--- public/chart.js
+++ public/chart.js
@@ -1,45 +1,48 @@
 $(function(){
      
-   // console.log(UserAnalytics().find({}) )
    var guestCookieID = document.cookie.replace('meteorGuestMovieApp=','');
 
-   setTimeout(function(){
-    // Off set collecion find for chained undefined error 
-       var userData = (UserAnalytics.find({guestID: guestCookieID}).fetch()[0].genrecounter);
-   }, 100)
-   
-  
-    var testdata = [
-        {key: "One", y: 20},
-        {key: "Two", y: 20},
-        {key: "Three", y: 20},
-        {key: "Four", y: 20},
-        {key: "Five", y: 5},
-        {key: "Six", y: 5},
-        {key: "Seven", y: 10}
-    ];
+    // fetches user data from collection by GUEST ID then get the genrecounter attribute
+    var userData = UserAnalytics.find({guestID: guestCookieID}).fetch();
 
-    var height = 350;
-    var width = 350;
-    var chart1;
-    nv.addGraph(function() {
-        var chart1 = nv.models.pieChart()
-            .x(function(d) { return d.key })
-            .y(function(d) { return d.y })
-            .donut(true)
-            .width(width)
-            .height(height)
-            .padAngle(.08)
-            .cornerRadius(5)
-            .id('donut1'); // allow custom CSS for this one svg
-        chart1.title("100%");
-        chart1.pie.donutLabelsOutside(true).donut(true);
-        d3.select("#test1")
-            .datum(testdata)
-            .transition().duration(1200)
-            .call(chart1);
-        //nv.utils.windowResize(chart1.update);
-        return chart1;
-    });
+    $('#add-movie').on('click', function(){
+        userData =  UserAnalytics.find({guestID: guestCookieID}).fetch();
+        filterData = _.filter(userData,function(item){
+            return item.count > 0
+        })
+    })
+    if(userData){
+        userData = userData[0].genrecounter;
+        // Filter results for items only have count greater than zero
+        var filterData = _.filter(userData,function(item){
+            return item.count > 0
+        })
+
+        var height = 350;
+        var width = 350;
+        var chart1;
+        nv.addGraph(function() {
+            var chart1 = nv.models.pieChart()
+                .x(function(d) { 
+                    return d.genre 
+                })
+                .y(function(d) { return d.count })
+                .donut(true)
+                .width(width)
+                .height(height)
+                .padAngle(.08)
+                .cornerRadius(5)
+                .id('donut1'); // allow custom CSS for this one svg
+            chart1.title("100%");
+            chart1.pie.donutLabelsOutside(true).donut(true).labelType("percent") ;
+            d3.select("#test1")
+                .datum(filterData)
+                .transition().duration(1200)
+                .call(chart1);
+            //nv.utils.windowResize(chart1.update);
+            return chart1;
+        });
+    }
+  
 })
   
